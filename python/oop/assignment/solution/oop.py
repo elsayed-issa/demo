552e167e696525d1573b84d2ba24f09b70b1be0a
Elsayed Issa <elsayed.sabry.issa@gmail.com>
2023-01-11 13:10:09 -0700

    add oop

---
+++ python/oop/assignment/solution/oop.py
@@ -0,0 +1,44 @@
+#from typing import Text
+
+import numpy as np
+import re
+import matplotlib.pylab as plt
+
+
+### ADD IMPORTS HERE
+
+
+class Alice:
+    """Docs and instructions to be added"""
+    def __init__(self, filename):
+        self.filename = filename
+        # YOUR CODE HERE
+        raise NotImplementedError
+        
+
+    def read_file(self):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+    def clean_text(self):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+    def split_text(self):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+    def odd_even_words(self, even_odd):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+    def frequency(self, num):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+    def plot(self):
+        # YOUR CODE HERE
+        raise NotImplementedError
+
+
+
